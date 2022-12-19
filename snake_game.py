c8af34a85337e70d35c77083889f4f315df3fa5e
hopepisode <hopepisode@gmail.com>
2022-12-19 15:35:33 +0900

    스네이크 움직이기 & 먹이 생성

--- snake_game.py
+++ snake_game.py
@@ -21,30 +21,48 @@ pygame.display.update() #업데이트
 #Loop until the user clicks the close button.
 fps = pygame.time.Clock() # 프레임
 
-# 뱀의 초기 좌표값
+# 뱀의 머리 좌표값
 first=[200,20]
+second=[first[0]-20,first[1]]
+snake_position=[first,[first[0]-20,first[1]],[first[0]-40,first[1]],[first[0]-60,first[1]]]
 # 함수 생성
 
+# 먹이 생성 함수
+def apple():
+    pygame.draw.rect(screen, RED,[120,120,20,20],0)
 # 스네이크 생성 함수
 
-def snake():
-    pygame.draw.rect(screen, BLACK,[first[0],first[1],20,20],0)
-    pygame.draw.rect(screen, BLACK,[first[0]-20,first[1],20,20],0)
-    pygame.draw.rect(screen, BLACK,[first[0]-40,first[1],20,20],0)
-    pygame.draw.rect(screen, BLACK,[first[0]-60,first[1],20,20],0)
+def snake(position):
+    pygame.draw.rect(screen, BLACK,[position[0],position[1],20,20],0)
 
-#블럭을 움직이는 함
+
+# 뱀의 이동할 때 앞에 있는 뱀의 위치를 저장하는 함수
+# 뱀의 첫번째 부분이 한 칸 이동하면 뱀의 두번째 부분이 첫번째 부분이 있던 위치로 바로 이동한다.
+# 배열의 역순으로 반복문을 실행해서 위치좌표를 한 칸씩 밀어낸다?
+def follow_head(position):
+    for i in range(len(position),1,-1):
+        position[i-1][0]=position[i-2][0]
+        position[i-1][1]=position[i-2][1]
+
+#블럭을 움직이는 함수
 
 def move_block():
     if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_UP:       
-            first[1] -= 20         # 블록의 y 좌표를 1 뺀다
-        elif event.key == pygame.K_DOWN:   
-            first[1] += 20         # 블록의 y 좌표를 1 더한다
-        elif event.key == pygame.K_LEFT:   
-            first[0] -= 20         # 블록의 x 좌표를 1 뺀다
-        elif event.key == pygame.K_RIGHT:  
-            first[0] += 20         # 블록의 x 좌표를 1 더한다
+        if event.key == pygame.K_UP:
+            # snake_position[1][0]=snake_position[0][0]
+            # snake_position[1][1]=snake_position[0][1]
+            follow_head(snake_position)
+            snake_position[0][1] -= 20         # 블록의 y 좌표를 20 뺀다
+        elif event.key == pygame.K_DOWN:
+            follow_head(snake_position)
+            snake_position[0][1] += 20         # 블록의 y 좌표를 20 더한다
+        elif event.key == pygame.K_LEFT:
+            follow_head(snake_position)
+            snake_position[0][0] -= 20         # 블록의 x 좌표를 20 뺀다
+        elif event.key == pygame.K_RIGHT:
+            follow_head(snake_position)
+            snake_position[0][0] += 20         # 블록의 x 좌표를 20 더한다
+
     
 # 게임하는 동안 작동하는 함수
 def rungame():
@@ -52,7 +70,9 @@ def rungame():
     while running:
         fps.tick(10) #초당 10프레임?로 재
         screen.fill(WHITE)
-        snake()
+        for i in range(len(snake_position)):
+            snake(snake_position[i])
+        apple()
         for event in pygame.event.get(): #이벤트의 발생 여부에 따른 반복문 -> 중간에 발생한 이벤트를 캐치하고 검사
             move_block()    #블럭을 움직이는 함수
             if event.type == pygame.QUIT: #창을 닫는 이벤트 발생했는가?
@@ -60,7 +80,6 @@ def rungame():
             
         # This MUST happen after all the other drawing commands.
         pygame.display.flip()   #update 와 비슷하지만 flip은 전체 surface를 업데이트, update는 특정 부분 가능
-
 
 rungame()
 pygame.quit()
